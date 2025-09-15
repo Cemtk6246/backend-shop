eab0b1dd5f93e91376f4dcc738c7c3064d8ae4ee
Nurhak Tuekek <nurhak.tuekek@c24.de>
2025-09-15 11:36:40 +0200

    first initial commit regarding our web service

---
+++ src/test/kotlin/own/little/project/olp/OlpApplicationTests.kt
@@ -0,0 +1,13 @@
+package own.little.project.olp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OlpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
